6c9b188a94c99ef22b4094303106512fcf701d1f
expinc <expinc@outlook.com>
2022-04-05 14:36:13 +0800

    Add test cases of updating trades

--- finclerk/journal.py
+++ finclerk/journal.py
@@ -27,7 +27,7 @@ def add_product(account_id, code, name, type) -> Product:
         account = Account.select().where(Account.id == account_id).get()
         return Product.create(account=account, code=code, name=name, type=type)
 
-def add_trade(product_id, side, price, quantity, date) -> Trade:
+def _validate_trade(side, price, quantity, date):
     if not re.match(datetime_pattern, date):
         raise Exception("Invalid date format: {}, expects: YYYY-MM-DD".format(date))
     if side not in trade_sides:
@@ -37,6 +37,8 @@ def add_trade(product_id, side, price, quantity, date) -> Trade:
     if quantity <= 0:
         raise Exception("Quantity must be positive")
 
+def add_trade(product_id, side, price, quantity, date) -> Trade:
+    _validate_trade(side, price, quantity, date)
     with db.database.atomic():
         product = Product.select().where(Product.id == product_id).get()
         return Trade.create(product=product, side=side, price=price, quantity=quantity, datetime=date)
@@ -45,12 +47,14 @@ def get_trades_of_product(product_id) -> List[Trade]:
     with db.database.atomic():
         return Trade.select().where(Trade.product_id == product_id)
 
-def update_trade(id, side, price, quantity) -> int:
+def update_trade(id, side, price, quantity, date) -> int:
+    _validate_trade(side, price, quantity, date)
     with db.database.atomic():
         trade = Trade.select().where(Trade.id == id).get()
         trade.side = side
         trade.price = price
         trade.quantity = quantity
+        trade.datetime = date
         return trade.save()
 
 def delete_trade(id) -> int:
---
+++ test/journal/test_update_trade.py
@@ -0,0 +1,61 @@
+import peewee
+from test import common
+from assertpy import assert_that
+from finclerk import journal
+from finclerk import model
+
+class TestJournalUpdateTrade:
+
+    @classmethod
+    def setup_class(cls):
+        common.init_app()
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_ordinary(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+
+        cnt_updated = journal.update_trade(trade.id, "SELL", 11.9, 200.5, "2022-01-31")
+        assert_that(cnt_updated).is_equal_to(1)
+
+        new_trade = model.Trade.select().where(model.Trade.id == trade.id).get()
+        assert_that(new_trade.side).is_equal_to("SELL")
+        assert_that(new_trade.price).is_equal_to(11.9)
+        assert_that(new_trade.quantity).is_equal_to(200.5)
+        assert_that(new_trade.datetime).is_equal_to("2022-01-31")
+
+    def test_no_change(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+
+        cnt_updated = journal.update_trade(trade.id, trade.side, trade.price, trade.quantity, trade.datetime)
+        assert_that(cnt_updated).is_equal_to(1)
+
+        new_trade = model.Trade.select().where(model.Trade.id == trade.id).get()
+        assert_that(new_trade.side).is_equal_to(trade.side)
+        assert_that(new_trade.price).is_equal_to(trade.price)
+        assert_that(new_trade.quantity).is_equal_to(trade.quantity)
+        assert_that(new_trade.datetime).is_equal_to(trade.datetime)
+
+    def test_invalid_side(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+        assert_that(journal.update_trade).raises(Exception).when_called_with(trade.id, "NONE", trade.price, trade.quantity, trade.datetime)
+
+    def test_invalid_price(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+        assert_that(journal.update_trade).raises(Exception).when_called_with(trade.id, trade.side, -10.9, trade.quantity, trade.datetime)
+
+    def test_invalid_quantity(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+        assert_that(journal.update_trade).raises(Exception).when_called_with(trade.id, trade.side, trade.price, 0, trade.datetime)
+
+    def test_invalid_date(self):
+        trade = journal.add_trade(1, "BUY", 10.9, 100.5, "2022-01-01")
+        assert_that(trade).is_not_none()
+        assert_that(journal.update_trade).raises(Exception).when_called_with(trade.id, trade.side, trade.price, trade.quantity, "INVALID")
